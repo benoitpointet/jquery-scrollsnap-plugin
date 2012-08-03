be0f69bf60ad20db5adbca2226fe2f35e84dfb0e
Benoit Pointet <benoit.pointet@liip.ch>
2012-08-03 19:06:10 +0200

    first implementation

--- src/jquery.scrollsnap.js
+++ src/jquery.scrollsnap.js
@@ -1,5 +1,23 @@
 (function( $ ) {
-  $.fn.scrollsnap = function() {
-    // Do your awesome plugin stuff here
-  };
+    $.fn.scrollsnap = function() {
+
+        return this.each(function() {
+            var y = 0,
+                $this = this;
+
+             $(document).scroll(function (e) {
+                var y2 = $this.offsetTop - window.scrollY,
+                    distance = Math.abs(y2),
+                    crossed = y * y2 < 1;
+
+                if (distance < 20) {
+                    window.scrollTo(window.scrollX, $this.offsetTop);
+                }
+
+                //console.log($($this).html(), distance);
+
+                y = y2;
+            });
+        });
+    };
 })( jQuery );
